fb92a53a5ec05b25b785da50376f7535c09ac043
gurubrahmam <vgurubrahmam338@gmail.com>
2025-01-09 08:32:43 +0530

    changes in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,19 @@ import {
   CarouselPrevious,
 } from "../src/components/ui/carousel";
 import { cardItems, trending_products } from "./lib/utils";
-import { ChevronLeft, ChevronRight, Instagram, Linkedin, } from "lucide-react";
+import { ChevronLeft, ChevronRight, Instagram, Linkedin } from "lucide-react";
 
-function App() {
-
+const App: React.FC = () => {
   return (
     <>
       <div className="w-full">
-        <Header/>
-        
-        <div className=" w-full py-8 px-8 ">
+        <Header />
+        <div className="w-full py-8 px-8">
           <img src="src/assets/homepage.png" className="w-full h-[90%]" />
           <h1 className="font-bold text-3xl text-center py-3 max-sm:text-xl">
             Product Categories
           </h1>
-          <div className="w-full h-full  py-3  max-sm:py-0">
+          <div className="w-full h-full py-3 max-sm:py-0">
             <Carousel
               opts={{
                 align: "center",
@@ -39,14 +37,14 @@ function App() {
                     <div className="p-1">
                       <Card className="w-[95%] bg-[#D3D3D3] h-full">
                         <CardContent className="flex aspect-square justify-center gap-y-8 h-full p-10 max-sm:p-4">
-                          <span className="text-md  font-bold max-sm:text-sm">
+                          <span className="text-md font-bold max-sm:text-sm">
                             <h3 className="text-black text-center">
                               {item.title}
                             </h3>
                             <div className="w-full h-full flex items-end max-sm:items-center">
                               <img
                                 src={item.img}
-                                className="w-24 h-24 max-sm:w-16 max-sm:h-16 "
+                                className="w-24 h-24 max-sm:w-16 max-sm:h-16"
                               />
                             </div>
                           </span>
@@ -62,10 +60,10 @@ function App() {
           </div>
           <div className="bg-[#D3D3D3] w-full h-14 rounded-xl my-10 flex items-center justify-start gap-4 p-4">
             <img src="src/assets/segment.png" />
-            <h1 className=" font-bold"> Segments</h1>
+            <h1 className="font-bold"> Segments</h1>
           </div>
           <div className="flex items-center justify-center h-full gap-4 w-full max-sm:flex-col">
-            <div className="w-full  flex flex-col items-center bg-[url('https://media.istockphoto.com/id/1450159186/photo/a-white-modern-farmhouse.jpg?s=612x612&w=0&k=20&c=X_ZhjuMI0dWL2kj4ycvjRATivPPoHucaW8RBlXp2Pz8=')] bg-cover bg-center h-[250px] rounded-md justify-between py-8">
+            <div className="w-full flex flex-col items-center bg-[url('https://media.istockphoto.com/id/1450159186/photo/a-white-modern-farmhouse.jpg?s=612x612&w=0&k=20&c=X_ZhjuMI0dWL2kj4ycvjRATivPPoHucaW8RBlXp2Pz8=')] bg-cover bg-center h-[250px] rounded-md justify-between py-8">
               <h3 className="text-center font-bold">Residential</h3>
               <button className="bg-black text-white px-4 py-2 rounded">
                 View More
@@ -87,9 +85,9 @@ function App() {
           <div className="bg-[#D3D3D3] w-full h-14 rounded-xl my-10 flex items-center justify-between gap-4 p-4 max-sm:p-2">
             <div className="flex items-center justify-start gap-4 p-4 max-sm:p-0 max-sm:gap-3">
               <img src="src/assets/trending_products.png" />
-              <h3 className=" font-bold max-sm:text-xs"> Trending products</h3>
+              <h3 className="font-bold max-sm:text-xs"> Trending products</h3>
             </div>
-            <div className="flex items-center justify-end gap-1 p-4  max-sm:p-0 max-sm:gap-0">
+            <div className="flex items-center justify-end gap-1 p-4 max-sm:p-0 max-sm:gap-0">
               <ChevronLeft />
               <ChevronRight />
             </div>
@@ -131,7 +129,7 @@ function App() {
                 <CarouselItem key={index} className="w-[80%] h-[80%] mx-auto">
                   <div className="p-1">
                     <div className="w-full h-full">
-                      <CardContent className="flex aspect-video items-center justify-center  w-full h-full">
+                      <CardContent className="flex aspect-video items-center justify-center w-full h-full">
                         <img
                           src={item.img}
                           className="rounded-lg w-full h-full"
@@ -146,7 +144,7 @@ function App() {
             <CarouselNext />
           </Carousel>
         </div>
-        <div className="w-full h-full  flex flex-col items-center justify-center gap-4 py-10">
+        <div className="w-full h-full flex flex-col items-center justify-center gap-4 py-10">
           <h1 className="font-bold text-3xl">About us</h1>
           <p className="text-center text-[#030303] text-md w-[80%]">
             Discover the epitome of luxury living with our exclusive furniture
@@ -154,7 +152,7 @@ function App() {
             sophistication and elegance to your home.
           </p>
           <div className="flex w-full h-full items-start gap-4 max-sm:flex-wrap">
-            <div className="grid  md:grid-cols-2 lg:grid-cols-2  gap-4 w-[50%] max-sm:w-full max-sm:grid-cols-1   max-sm:p-5 p-10">
+            <div className="grid md:grid-cols-2 lg:grid-cols-2 gap-4 w-[50%] max-sm:w-full max-sm:grid-cols-1 max-sm:p-5 p-10">
               <div>
                 <h3 className="font-bold text-2xl">Inspired Living</h3>
                 <p>
@@ -187,7 +185,7 @@ function App() {
                 </p>
               </div>
             </div>
-            <div className="grid grid-cols-2 md:grid-cols-2 w-[50%] gap-4 max-sm:w-full  max-sm:px-5 ">
+            <div className="grid grid-cols-2 md:grid-cols-2 w-[50%] gap-4 max-sm:w-full max-sm:px-5">
               <img src="src/assets/img1.png" className="w-fit h-fit" />
               <img src="src/assets/img2.png" className="w-fit max-h-fit" />
               <img src="src/assets/img3.png" className="w-fit h-fit mt-0 space-y-0" />
@@ -289,7 +287,7 @@ function App() {
             Why Vibrer Stands Out?
           </h1>
           <div className="flex flex-wrap justify-center items-center gap-5 min-h-screen py-5 px-5">
-            <div className="w-[438px] h-[320px] bg-[#D3D3D3]  rounded-xl flex flex-col items-center justify-end gap-4">
+            <div className="w-[438px] h-[320px] bg-[#D3D3D3] rounded-xl flex flex-col items-center justify-end gap-4">
               <img src="src/assets/collections.png" />
               <div className="p-5">
                 <p className="font-bold">500 + Collections</p>
@@ -299,7 +297,7 @@ function App() {
                 </p>
               </div>
             </div>
-            <div className="w-[438px] h-[320px] bg-[#D3D3D3]  rounded-xl flex flex-col items-center justify-end gap-4">
+            <div className="w-[438px] h-[320px] bg-[#D3D3D3] rounded-xl flex flex-col items-center justify-end gap-4">
               <img
                 src="https://www.figma.com/file/MTcDtv29awUDXFVDAnwnVR/image/1adab27a0bee11f475e8fd0393fd197b03d089b7"
                 className="h-[28%]"
@@ -313,7 +311,7 @@ function App() {
               </div>
             </div>
 
-            <div className="w-[438px] h-[320px] bg-[#D3D3D3]  rounded-xl flex flex-col items-center justify-end gap-4">
+            <div className="w-[438px] h-[320px] bg-[#D3D3D3] rounded-xl flex flex-col items-center justify-end gap-4">
               <img src="src/assets/top-nouch.png" className="h-[28%]" />
               <div className="p-5">
                 <p className="font-bold">Top-notch Quality</p>
@@ -324,7 +322,7 @@ function App() {
               </div>
             </div>
 
-            <div className="w-[438px] h-[320px] bg-[#D3D3D3]  rounded-xl flex flex-col items-center justify-end gap-4">
+            <div className="w-[438px] h-[320px] bg-[#D3D3D3] rounded-xl flex flex-col items-center justify-end gap-4">
               <img src="src/assets/coustomization.png" className="h-[28%]" />
               <div className="p-5">
                 <p className="font-bold">Colour and Length customization</p>
@@ -336,9 +334,9 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="bg-black h-auto p-8 text-white ">
+        <div className="bg-black h-auto p-8 text-white">
           <div className="flex w-full max-sm:flex-col items-start justify-between gap-4">
-            <div className="w-[50%] max-sm:w-full flex flex-col items-start gap-4">
+            <div className="w-[80%] max-sm:w-full flex flex-col items-start gap-4">
               <img
                 src="https://www.figma.com/file/MTcDtv29awUDXFVDAnwnVR/image/d7fb35d10a5d5473989ba4ec575b8af8fe1f746c"
                 className="w-20 h-20 mx-2 my-2"
@@ -360,7 +358,7 @@ function App() {
               </div>
             </div>
 <div className="flex flex-row w-full  items-start justify-center gap-4  ">
-            <div className="w-[50%] text-white items-center justify-center place-items-start max-sm:w-full">
+            <div className="w-[20%] text-white items-center justify-center place-items-start max-sm:w-full">
               <p >Home</p>
 
               <p>Living</p>
